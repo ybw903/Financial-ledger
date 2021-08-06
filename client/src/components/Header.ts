4db4cbf35a5a6bd5f4e97c0c74d42c45869c86df
ybw903 <conner903@naver.com>
2021-08-06 12:30:23 +0900

    feat: header

--- client/src/components/Header.ts
+++ client/src/components/Header.ts
@@ -7,6 +7,8 @@ export default class Header {
     render():void {
         document.querySelector('header').innerHTML=`
             ${this.createNavigator()}
+            ${this.createMonthSelector()}
+            ${this.createSumIndicator()}
         `;
     }
 
@@ -26,6 +28,43 @@ export default class Header {
                     차트버튼
                 </div>
             </nav>
+        `
+    }
+    createMonthSelector():string {
+        return `
+            <div>
+                <div>
+                    ${this.craeteMonthIndicator(2021,7)}
+                    ${this.craeteMonthIndicator(2021,8)}
+                    ${this.craeteMonthIndicator(2021,9)}
+                </div>
+                <div></div>
+                <div></div>
+            </div>
+        `
+    }
+
+    craeteMonthIndicator(year: number, month: number):string {
+        return `
+            <div>
+                <div>${year}</div>
+                <div>${month}</div>
+            <div>
+        `
+    }
+
+    createSumIndicator(): string {
+        return `
+            <div>
+                <div>
+                    <div>
+                        +
+                    </div>
+                    <div>
+                        -
+                    </div>
+                </div>
+            </div>
         `
     }
 }
---
+++ client/src/types.ts
@@ -0,0 +1,7 @@
+export interface CalanderDayDate {
+    date: number
+    isInThisMonth: boolean
+    isHoliday: boolean
+    incomeSum: number
+    expenditureSum: number
+}
