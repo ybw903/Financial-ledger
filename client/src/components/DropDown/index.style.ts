a997c68af8d66859a3eff33264d17b96525f33c5
ybw903 <conner903@naver.com>
2022-02-03 22:31:10 +0900

    style: box shadow

--- client/src/components/DropDown/index.style.ts
+++ client/src/components/DropDown/index.style.ts
@@ -8,4 +8,5 @@ export const DropDownList = styled.div<IDropDownList>`
   position: absolute;
   top: 66px;
   display: ${(props) => (props.isShow ? 'block' : 'none')};
+  box-shadow: 10px 10px 25px gray;
 `
